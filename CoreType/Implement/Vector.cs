708fe5654537a349b471b8445faad742a5ebe689
Jervis <cuijervis@126.com>
2015-09-12 14:08:37 +0800

    DataStructureByCS
    DataStructure Code by CSharp

---
+++ CoreType/Implement/Vector.cs
@@ -0,0 +1,14 @@
+﻿using CoreType.Define;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreType.Implement
+{
+    public class Vector : IVector
+    {
+
+    }
+}
