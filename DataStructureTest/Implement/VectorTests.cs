08105a2b429ea38f2e89d6274d8f1381f75f7c77
Jervis <cuijervis@126.com>
2015-09-13 17:42:57 +0800

    update

---
+++ DataStructureTest/Implement/VectorTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CoreType.Implement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreType.Implement.Tests
+{
+    [TestClass()]
+    public class VectorTests
+    {
+        [TestMethod()]
+        public void VectorTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VectorTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VectorTest2()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VectorTest3()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void VectorTest4()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SizeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FindTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisorderedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SortTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeduplicateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UniqufiyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TraverseTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
