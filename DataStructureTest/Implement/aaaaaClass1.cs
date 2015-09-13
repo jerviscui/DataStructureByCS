4b79aea65c07b73effabd53e8b7b1983361abef0
Jervis <cuijervis@126.com>
2015-09-13 17:57:41 +0800

    Revert "Revert "a ha, i created a class""
    This reverts commit 05bf8a09da0cf88378491bbad06e1bd821e6ac29.

---
+++ DataStructureTest/Implement/aaaaaClass1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureTest.Implement
+{
+    class aaaaaClass1
+    {
+    }
+}
