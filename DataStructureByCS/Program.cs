4aa6e3899fb3ea6d2bc02f2b49a28558d3091499
Jervis <cuijervis@126.com>
2015-09-21 18:09:17 +0800

    update

--- DataStructureByCS/Program.cs
+++ DataStructureByCS/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,30 @@ namespace DataStructureByCS
                 Console.WriteLine(i);
             }
 
-            MyList<int> aaa = new MyList<int>();
-            aaa.InsertAsFirst(10);
-            
-            var ccc = new MyList<int>(aaa, 0, 1);
-            Console.WriteLine(aaa.Last().Data);
+			Test aaa = new Test();
+	        aaa.Data = 10;
+	        Add(aaa);
+			Console.WriteLine(aaa.Data);
+			Add2(aaa);
+			Console.WriteLine(aaa.Data);
 
             Console.ReadLine();
         }
+
+	    private static void Add(Test a)
+	    {
+		    a.Data = 11;
+	    }
+
+		private static void Add2(Test a)
+		{
+			a = new Test();
+			a.Data = 12;
+		}
     }
+
+	public class Test
+	{
+		public int Data { get; set; }
+	}
 }
